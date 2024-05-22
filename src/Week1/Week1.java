ab0b2b6f8c40e8f1e334d7402cc5634d4007b7f9
NeonBlueSky <sanatarat@outlook.com>
2024-05-22 18:57:39 -0500

    Test

--- src/Week1/Week1.java
+++ src/Week1/Week1.java
@@ -1,7 +1,21 @@
 package Week1;
 
+import java.util.Scanner;
 public class Week1 {
     public static void main(String[] args) {
-        System.out.println("Week 1");
+        System.out.println("Week 1 Odd or Even Project Test");
+
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.print("Enter a number: ");
+        int number = scanner.nextInt();
+
+        if (number % 2 == 0) {
+            System.out.println(number + " is even.");
+        } else {
+            System.out.println(number + " is odd.");
+        }
+
+        scanner.close();
     }
 }
