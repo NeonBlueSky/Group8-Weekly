7ba33e3f162767a5d100abc7b490f7c526cf292d
ivangg75 <ivangg75@yahoo.com>
2024-05-24 16:53:21 -0400

    OddOrEven renamed to EvenNumber.java

--- src/Week1/EvenNumber.java
+++ src/Week1/EvenNumber.java
@@ -2,7 +2,7 @@ package Week1;
 
 import java.util.Scanner;
 
-public class OddOrEven {
+public class EvenNumber {
     public static void main(String[] args) {
 
         System.out.println("Enter a number");
