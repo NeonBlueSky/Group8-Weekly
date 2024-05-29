cee5698adca9944c6f809d3740cc435bb4a6f04c
nazar <imnazar98@gmail.com>
2024-05-29 10:22:23 -0500

    Created method which can identifies given number is even or odd

--- src/Week1/nazarii/PracticeTask.java
+++ src/Week1/nazarii/PracticeTask.java
@@ -1,11 +1,25 @@
 package Week1.nazarii;
+/* Write a method which can identifies given number is even or odd
+Example:
+        identify(5) -> "Odd"
+        identify(6) -> "Even"
+ */
 
 public class PracticeTask {
 
-    public static void main(String[] args) {
+        public static String EvenOrOdd(int number) {
+            if (number % 2 == 0) {
+                return "Even";
+            } else {
+                return "Odd";
+            }
+        }
 
+        public static void main(String[] args) {
+            int number = 6;
+            String result = EvenOrOdd(number);
 
-
+            System.out.println("Number " + number + " is " + result);
+        }
     }
 
-}
