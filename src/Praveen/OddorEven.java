67ee80f293240e2d68695430d51e479866304cac
prave <praveenk0727@gmail.com>
2024-05-29 17:10:53 -0400

    Praveen week 1

---
+++ src/Praveen/OddorEven.java
@@ -0,0 +1,17 @@
+package Praveen;
+
+
+public class OddorEven {
+    public static void main(String[] args) {
+        int number = 25;
+        identifyNumber(number);
+    }
+
+    public static void identifyNumber(int number) {
+        if (number % 2 == 0) {
+            System.out.println(number + " is an even number.");
+        } else {
+            System.out.println(number + " is an odd number.");
+        }
+    }
+}
