c0d85b9105597b4fbd95feaeaa22d440c6956ff3
veraPelda <akmazpelda@gmail.com>
2024-06-05 09:17:11 -0500

    PeldaHanar week1

---
+++ src/PeldaHanar/week1/divisionWithoutOperator.java
@@ -0,0 +1,8 @@
+package PeldaHanar.week1;
+
+public class divisionWithoutOperator {
+
+    //Write a method that can divide two numbers without using divison operator
+
+    // solutions
+}
---
+++ src/PeldaHanar/week1/oddOrEven.java
@@ -0,0 +1,9 @@
+package PeldaHanar.week1;
+
+public class oddOrEven {
+
+    //Write a method which can identifies given number is even or odd
+
+    // solutions
+
+}
