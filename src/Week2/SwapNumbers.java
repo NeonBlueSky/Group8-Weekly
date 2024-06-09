547cea02c31880292cbbd749b76f7933d24de76e
prave <praveenk0727@gmail.com>
2024-06-09 11:59:29 -0400

    Praveen week 2 Swap

---
+++ src/Week2/SwapNumbers.java
@@ -0,0 +1,21 @@
+package Week2;
+
+public class SwapNumbers {
+    public static void main(String[] args) {
+        int a = 10;
+        int b = 20;
+
+        System.out.println("Before swapping:");
+        System.out.println("a = " + a);
+        System.out.println("b = " + b);
+
+        // Swapping without using a third variable
+        a = a + b;
+        b = a - b;
+        a = a - b;
+
+        System.out.println("After swapping:");
+        System.out.println("a = " + a);
+        System.out.println("b = " + b);
+    }
+}
