740fc88feba2c426447fc6a30a63db30ce0a6f1f
prave <praveenk0727@gmail.com>
2024-06-11 20:39:56 -0400

    Praveen week 2 DivisibleNumbers and ConsecutiveNumbers

--- src/Week2/DivisibleNumbers.java
+++ src/Week2/DivisibleNumbers.java
@@ -3,13 +3,24 @@ package Week2;
 
 public class DivisibleNumbers {
     public static void main(String[] args) {
+        System.out.println("Divisible By 15");
         for (int i = 1; i <= 100; i++) {
-            if (i % 15 == 0) {
-                System.out.println(i + " DivisibleBy15");
-            } else if (i % 3 == 0) {
-                System.out.println(i + " DivisibleBy3");
-            } else if (i % 5 == 0) {
-                System.out.println(i + " DivisibleBy5");
+            if (i % 15  == 0) {
+                System.out.print(i + " ");
+            }
+        }
+
+        System.out.println("\nDivisible By 5");
+        for (int i = 1; i <= 100; i++) {
+            if (i % 5 == 0 && i % 15 != 0) {
+                System.out.print(i + " ");
+            }
+        }
+
+        System.out.println("\nDivisible By 3");
+        for (int i = 1; i <= 100; i++) {
+            if (i % 3 == 0 && i % 15 != 0) {
+                System.out.print(i + " ");
             }
         }
     }
